bf5c01c219dbffe67a03d47b702cd1c291d07152
Sohyun An <90887813+cownow4425@users.noreply.github.com>
2021-12-14 19:41:20 +0900

    Create telegram.py

---
+++ shortest_path_algorithm/telegram.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+
+def dijstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+n, m, start = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c, = map(int, input().split())
+    graph[a].append((b, c))
+
+dijstra(start)
+
+count = 0
+max_distance = 0
+for d in distance:
+    if d != INF:
+        count += 1
+        max_distance = max(max_distance, d)
+print(count - 1, max_distance)
